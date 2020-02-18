aab681f62ffafe5e917d7e1660553aae3d4efd45
Pratik <pratik.patil01@sap.com>
2020-02-18 13:02:48 +0530

    deployment setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const request = require('./api');
 const appInfo = require('./credentials');
+const path = require('path');
 
 
 const app = express();
@@ -83,6 +84,13 @@ function fetchData(cities) {
         return Promise.all(promises);
 }
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 const port = process.env.PORT || 1506;
 
 app.listen(port, () => console.log(`Server started on port: ${port}`))
